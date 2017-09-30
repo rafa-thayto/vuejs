d28c67d3b0eb4a71d0f3bf5e0bd7a2a07d189fb7
Rafael Thayto <rafa.thayto@gmail.com>
2017-09-30 16:44:20 -0300

    Added babel in render-functions repository

--- vuejs-brasil/render-functions/src/main.js
+++ vuejs-brasil/render-functions/src/main.js
@@ -8,6 +8,36 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  template: '<App/>',
-  components: { App }
+  /* FORMAS DE RENDERIZAR */
+  // render: h => h(App)
+  
+  // template: '<App/>',
+  // components: { App }
+
+  // Bem mais interessante dessa forma
+  render (h) {
+    return h(App)
+  }
+
+/* 
+  render (createElement) {
+    // o createElement recebe 3 parâmetros
+    // 1 = tag html; 2 = options; 3 = array de filhos
+    // NÃO É RECOMENDÁVEL
+    return createElement('h1', {
+      attrs: {
+        id: 'rafael'
+      }
+      // Quando não tiver nenhum array de filhos você pode tirar o
+      // domProps e inserir ele diretamente
+      // domProps: {
+      //   innerHTML: 'Hello World'
+      // }
+    }, [
+      createElement('h1', 'Hello from h1'),
+      createElement('p', 'Hello from paragraphy')
+    ])
+  }
+ */
+  /* O "h" é um shorthand para $createElement*/
 })
