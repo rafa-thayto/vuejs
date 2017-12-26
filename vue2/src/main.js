bd8df856a5b6ace5966e665ca5e5f791c80a6200
Rafael Thayto <rafa.thayto@gmail.com>
2017-12-26 11:14:04 -0200

    Learn about data bind

--- vue2/src/main.js
+++ vue2/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
 
 new Vue({
   el: '#app',
-  render: h => h(App)
-})
+  data: {
+    content: {
+      header: 'Minha primeira aplicação Vue.js 2',
+      paragraph: 'Vuejs é foda pra krl Vuejs é foda pra krl Vuejs é foda pra krl Vuejs é foda pra krl Vuejs é foda pra krl Vuejs é foda pra krl Vuejs é foda pra krl'
+    }
+  }
+});
